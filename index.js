bd397aaddb4d6de3dcf491b8eaf5cfaea890f073
Alexander Ly <alexander.ly@jhuapl.edu>
2020-12-08 18:49:00 -0500

    uibuild

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const app = express()
 morgan.token('reqBody', (req, res) => JSON.stringify(req.body))
 
 app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :reqBody'))
 
